4adcc0548e3b5bf5e8d5d1a92a1ac8e7394c1b71
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-06-02 12:29:31 -0300

    Camada Model Entity em construção

---
+++ src/main/java/com/itb/inf2gm/pizzariabomdemais/model/entity/Usuario.java
@@ -0,0 +1,24 @@
+package com.itb.inf2gm.pizzariabomdemais.model.entity;
+
+
+import com.itb.inf2gm.pizzariabomdemais.model.enums.TipoUsuario;
+
+public class Usuario {
+
+    private Long id;
+    private String nome;
+    private String cpf;
+    private String email;
+    private String senha;
+    private String sexo;
+    private String logradouro;
+    private String cep;
+    private String bairro;
+    private String cidade;
+    private String uf;
+    private boolean codStatus;
+    private TipoUsuario tipoUsuario;
+
+
+
+}
---
+++ src/main/java/com/itb/inf2gm/pizzariabomdemais/model/enums/TipoUsuario.java
@@ -0,0 +1,9 @@
+package com.itb.inf2gm.pizzariabomdemais.model.enums;
+
+public enum TipoUsuario {
+
+    ADMIN,
+    CLIENTE,
+    FUNCIONARIO
+
+}
