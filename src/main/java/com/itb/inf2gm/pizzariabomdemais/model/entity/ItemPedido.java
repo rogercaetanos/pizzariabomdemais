4ba95c18eebf6dfc529235a72e13a1c455d3c590
Rogério Caetano dos Santos <rogerio.santos@docente.fieb.edu.br>
2026-06-16 08:37:41 -0300

    Camada model entity em construção

---
+++ src/main/java/com/itb/inf2gm/pizzariabomdemais/model/entity/ItemPedido.java
@@ -0,0 +1,43 @@
+package com.itb.inf2gm.pizzariabomdemais.model.entity;
+
+import java.math.BigDecimal;
+
+public class ItemPedido {
+
+    private Long id;
+    private int quantidadeItem;
+    private BigDecimal valorUnitario;
+    private boolean codStatus;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantidadeItem() {
+        return quantidadeItem;
+    }
+
+    public void setQuantidadeItem(int quantidadeItem) {
+        this.quantidadeItem = quantidadeItem;
+    }
+
+    public BigDecimal getValorUnitario() {
+        return valorUnitario;
+    }
+
+    public void setValorUnitario(BigDecimal valorUnitario) {
+        this.valorUnitario = valorUnitario;
+    }
+
+    public boolean isCodStatus() {
+        return codStatus;
+    }
+
+    public void setCodStatus(boolean codStatus) {
+        this.codStatus = codStatus;
+    }
+}
